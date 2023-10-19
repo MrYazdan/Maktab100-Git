57c414cc9b944cf0d1ef0aeaafad5a7de39b8f2b
Reza Yazdani <mrrezayazdani@yahoo.com>
2023-10-19 17:29:21 +0330

    [ Feature ] - [ #11 ] - add welcome message

--- calc/main.py
+++ calc/main.py
@@ -1,19 +1,2 @@
-num1 = float(input("Number 1 : "))
-num2 = float(input("Number 2 : "))
-operator = input("operator : ")
-
-if operator == "+":
-    print(num1+num2)
-elif operator == "-":
-    print(num1-num2)
-elif operator == "//":
-    print(num1//num2)
-elif operator == "/":
-    if num2==0:
-        raise ZeroDivisionError
-    print("Div:", num1/num2)
-    print("-" * 100)
-elif operator == "*":
-    print(num1*num2)
-else:
-    raise ValueError("Operation has invalid !")
+print("=" * 50, "Wellcome to Python Calculator".center(50), "\t\t  Version: 0.1", "=" * 50, sep="\n")
+print(eval(input("> ")))
