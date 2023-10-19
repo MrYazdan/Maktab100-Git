98f8b6e02ae4aa6ccf6d87ea555bc43dbaa45e39
MrYazdan <mrrezayazdani@yahoo.com>
2023-10-19 14:34:35 +0330

    Add | main.py

---
+++ calc/main.py
@@ -0,0 +1,14 @@
+num1 = int(input("Number 1 : "))
+num2 = int(input("Number 2 : "))
+operator = input("operator : ")
+
+if operator == "+":
+    print(num1+num2)
+elif operator == "-":
+    print(num1-num2)
+elif operator == "/":
+    print(num1/num2)
+elif operator == "*":
+    print(num1*num2)
+else:
+    raise ValueError("Operation has invalid !")
